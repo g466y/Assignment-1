e6e71e07ee3ca1d2534aa2ac630264e17533e591
Gabriella <gabcap1@gmail.com>
2015-03-01 22:46:41 -0500

    Commit

---
+++ assignment1.js
@@ -0,0 +1,8 @@
+(function () {
+
+var firstArray = ["cheese and chocolate", "running and rockclimbing", "boggle and scrabble"];
+function printList(input) {
+	console.log("I like,", input);
+}
+firstArray.forEach(printList);
+}());
